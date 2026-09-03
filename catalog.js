27423c6dcbe6c1cfebbb27c7555ac01916b91780
dvargas4 <dvg7103@gmail.com>
2026-09-03 16:44:42 -0600

    Update catalog.js

--- catalog.js
+++ catalog.js
@@ -13,7 +13,7 @@ window.STORE_CONFIG = {
   sheetRefreshMinutes: 5,
   freeDeliveryKg: 5,
   deliveryDay: "sábado",
-  deliveryLimit: "11:30 a. m.",
+  deliveryLimit: "01:00 a. m.",
   orderCutoff: "viernes",
   deliveryZone: "Alrededores de Calzada México-Xochimilco 62, San Lorenzo Huipulco, Tlalpan, Ciudad de México.",
   paymentOptions: ["Transferencia", "Tarjeta", "Efectivo"],
